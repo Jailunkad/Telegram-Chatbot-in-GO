e6a944b6ff6267ae11338636bbd72a4c88cc0e69
JaiLunkad <Jailunkad@hotmail.com>
2020-06-05 23:57:59 +0530

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Jailunkad/CSCI169-
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"os"
+	"log"
+	"github.com/yanzay/tbot/v2"
+	"github.com/joho/godotenv"
+)
+
+type score struct {
+	losses, draws, wins uint
+}
+
+type application struct {
+	client *tbot.Client
+	score
+}
+
+var (
+	app     application
+	bot     *tbot.Server
+	token   string
+	options = map[string]string{
+		// choice : beats
+		"paper":    "rock",
+		"rock":     "scissors",
+		"scissors": "paper",
+	}
+)
+
+func init() {
+	e := godotenv.Load()
+	if e != nil {
+		log.Println(e)
+	}
+	token = os.Getenv("TELEGRAM_TOKEN")
+}
+
+func main() {
+	bot = tbot.New(token, tbot.WithWebhook("https://rochambeau-bot.herokuapp.com", ":"+os.Getenv("PORT")))
+	app.client = bot.Client()
+	bot.HandleMessage("/start", app.startHandler)
+	bot.HandleMessage("/play", app.playHandler)
+	bot.HandleMessage("/score", app.scoreHandler)
+	bot.HandleMessage("/reset", app.resetHandler)
+	bot.HandleCallback(app.callbackHandler)
+	log.Fatal(bot.Start())
+}
